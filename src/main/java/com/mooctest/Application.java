ccaf0c970d966ed1dc1b1f600e6e20b520f2ae63
guochao <1021172957@qq.com>
2020-05-15 21:22:57 +0800

    change resources dictory

--- src/main/java/com/mooctest/Application.java
+++ src/main/java/com/mooctest/Application.java
@@ -2,6 +2,7 @@ package com.mooctest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @SpringBootApplication
 @EnableAsync
+@EnableConfigurationProperties
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
